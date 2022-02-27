ff9a276de6187b90291f4bd742582a68ae1e0fef
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2022-02-27 09:53:58 +0330

    feat : RequestParams.ts

---
+++ HotelListing_webAPI/Models/RequestParams.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelListing_webAPI.Models
+{
+    public class RequestParams
+    {
+        const int maxPageSize = 100;
+        public int PageIndex { get; set; } = 0;
+         private int _pageSize = 10;
+        public int PageSize { get { return _pageSize; } set { _pageSize = (value > maxPageSize) ? maxPageSize : value ; } }
+    }
+}
